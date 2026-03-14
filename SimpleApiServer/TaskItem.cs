c96d98c0cc1046e6c841b8f6152c497ab40e148a
Milena <koseevamilena3@gmail.com>
2026-03-14 15:00:01 +0600

    Добавлена фильтрация задач и улучшена обработка ошибок

--- SimpleApiServer/TaskItem.cs
+++ SimpleApiServer/TaskItem.cs
@@ -6,16 +6,16 @@ using System.Threading.Tasks;
 
 namespace SimpleApiServer
 {
-    
-        public class TaskItem
-        {
-            public int Id { get; set; }
+    public class TaskItem
+    {
+        public int Id { get; set; }
 
-            public string Title { get; set; }
+        public string Title { get; set; }
 
-            public string Description { get; set; }
+        public string Description { get; set; }
 
-            public bool IsCompleted { get; set; }
-        }
-    
+        public bool IsCompleted { get; set; }
+
+        public int Priority { get; set; }
+    }
 }
