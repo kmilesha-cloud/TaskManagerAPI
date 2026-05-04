0361b57384d1bef75f9fa01212eb6469877987cd
Milena <koseevamilena3@gmail.com>
2026-05-04 09:06:47 +0600

    feat: configuration logging and github actions

---
+++ SimpleApiServer/AppConfig.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace SimpleApiServer
+{
+    internal class AppConfig
+    {
+        public List<string> ListenUrls { get; set; } = new List<string>();
+        public string LogLevel { get; set; } = "Information";
+        public string LogFilePath { get; set; } = "logs/app.log";
+    }
+}
